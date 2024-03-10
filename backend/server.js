827b91a4044e78302441cc5ab59c287167192b01
Johnny <52947117+Toastjpg@users.noreply.github.com>
2024-03-09 22:11:35 -0800

    finish all db functions and endpoints for server

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,82 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send('this is my server')
+  res.send('hello world')
 })
 
-// app.get('/a', async (req, res) => {
-//   await db.helpers.addPerson('test', 123)
-//   let data = await db.helpers.getPeople()
-//   res.json(data)
-// })
+// get all recipes
+app.get('/recipes/all', async (req, res) => {
+  try {
+    const recipes = await db.getRecipes()
+    res.status(200).json(recipes)
+  }
+  catch (error) {
+    console.error('Error on GET /recipes/all :', error.message)
+    res.status(404).json({ error: 'Recipes not found' })
+  }
+})
 
+// get all ingredients for a recipe
+app.get('/ingredients/:recipe_id', async (req, res) => {
+  const recipe_id = req.params.recipe_id
 
+  try {
+    const ingredients = await db.getIngredients(recipe_id)
+    res.status(200).json(ingredients)
+  }
+  catch (error) {
+    console.error('Error on GET /ingredients/:recipe_id :', error.message)
+    res.status(404).json({ error: 'Ingredients for that recipe not found' })
+  }
+})
 
+// add new recipe
+app.post('/recipes', async (req, res) => {
+  const { recipe_name, directions, ingredients } = req.body
+
+  try {
+    const result = await db.addRecipe(recipe_name, directions, ingredients)
+    res.status(201).json({ message: 'Recipe added successfully', recipe_id: result })
+  }
+  catch (error) {
+    console.error('Error on POST /recipes : ', error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+// edit recipe
+app.put('/recipes/:recipe_id', async (req, res) => {
+  const recipe_id = req.params.recipe_id
+  const { recipe_name, directions, ingredients } = req.body
+
+  try {
+    const result = await db.editRecipe(recipe_id, recipe_name, directions, ingredients)
+    res.status(200).json({ message: 'Recipe edited successfully', recipe_id: result })
+  }
+  catch (error) {
+    console.error('Error on PUT /recipes/:recipe_id : ', error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+
+})
+
+// delete a recipe
+app.delete('/recipes/:recipe_id', async (req, res) => {
+  const recipe_id = req.params.recipe_id
+
+  try {
+    await db.deleteRecipe(recipe_id)
+    res.status(200).json({ message: 'Recipe deleted successfully' })
+
+  }
+  catch (error) {
+    console.error('Error on DELETE /recipes/:recipe_id : ', error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
 
 async function initDb() {
-  // await db.helpers.init()
-
-  // run helper script to create tables
-
+  await db.init()
 }
 
 // This starts up the server
--- backend/src/controllers/db.js
+++ backend/src/controllers/db.js
@@ -5,26 +5,168 @@ let pool = new Pool({
   connectionString: 'postgres://postgres:root@localhost:5432/recipe-app'
 })
 
-const helpers = {
-  init: async function () {
-    const q = 'CREATE TABLE IF NOT EXISTS temp(id SERIAL PRIMARY KEY, name VARCHAR(50), age INT)'
-    const res = await pool.query(q)
-  },
+async function init() {
+  const q = `
+    CREATE TABLE if NOT EXISTS recipes (
+      recipe_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      recipe_name VARCHAR(255) NOT NULL,
+      directions TEXT,
+      last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
 
-  getPeople: async function () {
-    const res = await pool.query('SELECT * FROM temp')
-    return res.rows
-  },
+    CREATE TABLE if NOT EXISTS ingredients (
+      ingredient_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      ingredient_name VARCHAR(255) NOT NULL
+    );
 
-  deleteById: async function (id) {
-    const q = 'DELETE FROM petempople WHERE id = $1'
-    const res = await pool.query(q, [id])
-  },
-
-  addPerson: async function (name, age) {
-    const q = 'INSERT INTO temp(name, age) VALUES($1, $2)'
-    const res = await pool.query(q, [name, age])
-  },
+    CREATE TABLE if NOT EXISTS recipe_to_ingredient (
+      recipe_id UUID REFERENCES recipes(recipe_id) ON DELETE CASCADE,
+      ingredient_id UUID REFERENCES ingredients(ingredient_id) ON DELETE CASCADE,
+      PRIMARY KEY (recipe_id, ingredient_id)
+    );
+  `
+  await pool.query(q)
 }
 
-module.exports = { helpers }
+async function getRecipes() {
+  try {
+    const q = 'SELECT * FROM recipes'
+    const res = await pool.query(q)
+    return res.rows
+  }
+  catch (e) {
+    throw e
+  }
+}
+
+async function getIngredients(recipe_id) {
+  try {
+    const getIngredientQuery = `
+      SELECT * FROM ingredients
+      WHERE ingredient_id IN
+      (SELECT ingredient_id FROM recipe_to_ingredient WHERE recipe_id = $1)
+    `
+    const res = await pool.query(getIngredientQuery, [recipe_id])
+    return res.rows
+  }
+  catch (e) {
+    throw e
+  }
+}
+
+// TODO: Potentially edit this to not add duplicate ingredients
+async function addRecipe(recipe_name, directions, ingredients) {
+  const client = await pool.connect()
+
+  try {
+    await client.query('BEGIN')
+
+    const insertRecipeQuery = 'INSERT INTO recipes (recipe_name, directions) VALUES ($1, $2) RETURNING recipe_id'
+    const recipeResult = await client.query(insertRecipeQuery, [recipe_name, directions])
+    const recipe_id = recipeResult.rows[0].recipe_id
+
+    // TODO this inserts all the ingredients for each recipe -> regardless if they alr exist in table
+    for (const ingredient_name of ingredients) {
+      const insertIngredientsQuery = 'INSERT INTO ingredients (ingredient_name) VALUES ($1) RETURNING ingredient_id'
+      let ingredientResult = await client.query(insertIngredientsQuery, [ingredient_name])
+      let ingredient_id = ingredientResult.rows[0].ingredient_id
+
+      const insertMappingsTable = 'INSERT INTO recipe_to_ingredient (recipe_id, ingredient_id) VALUES ($1, $2)'
+      await client.query(insertMappingsTable, [recipe_id, ingredient_id])
+    }
+
+    await client.query('COMMIT')
+    return recipe_id
+  }
+  catch (e) {
+    await client.query('ROLLBACK')
+    throw e
+  }
+  finally {
+    client.release()
+  }
+}
+
+// edit recipe given a recipe id
+// a bit complicated here implement this one last
+async function editRecipe(recipe_id, recipe_name, directions, ingredients) {
+  const client = await pool.connect()
+
+  try {
+    await client.query('BEGIN')
+
+    const updateRecipeQuery = `
+      UPDATE recipes SET recipe_name = $1, directions = $2, last_modified = CURRENT_TIMESTAMP
+      WHERE recipe_id = $3
+      RETURNING *
+    `
+    const updateResult = await client.query(updateRecipeQuery, [recipe_name, directions, recipe_id])
+
+    if (updateResult.rows.length === 0) {
+      throw new Error('Recipe not found')
+    }
+
+    const deleteOldIngredients = `
+      DELETE FROM ingredients
+      WHERE ingredient_id IN (SELECT ingredient_id FROM recipe_to_ingredient WHERE recipe_id = $1)
+    `
+    await client.query(deleteOldIngredients, [recipe_id]);
+
+    // Add updated ingredients
+    for (const ingredient_name of ingredients) {
+      const insertIngredientsQuery = 'INSERT INTO ingredients (ingredient_name) VALUES ($1) RETURNING ingredient_id'
+      let ingredientResult = await client.query(insertIngredientsQuery, [ingredient_name])
+      let ingredient_id = ingredientResult.rows[0].ingredient_id
+
+      const insertMappingsTable = 'INSERT INTO recipe_to_ingredient (recipe_id, ingredient_id) VALUES ($1, $2)'
+      await client.query(insertMappingsTable, [recipe_id, ingredient_id])
+    }
+
+    await client.query('COMMIT')
+    return recipe_id
+  }
+  catch (e) {
+    await client.query('ROLLBACK')
+    throw e
+  }
+  finally {
+    client.release()
+  }
+}
+
+async function deleteRecipe(recipe_id) {
+  const client = await pool.connect()
+
+  try {
+    await client.query('BEGIN')
+
+    // Delete recipe -> will cascade to mappings table
+    const deleteRecipeQuery = 'DELETE FROM recipes WHERE recipe_id = $1'
+    await client.query(deleteRecipeQuery, [recipe_id])
+
+    // Delete ingredients that are not in mapping table -> not referenced in any existing recipe
+    const deleteIngredientQuery = `
+      DELETE FROM ingredients
+      WHERE ingredient_id NOT IN (SELECT ingredient_id FROM recipe_to_ingredient)
+    `
+    await client.query(deleteIngredientQuery);
+
+    await client.query('COMMIT')
+  }
+  catch (e) {
+    await client.query('ROLLBACK')
+    throw e
+  }
+  finally {
+    client.release()
+  }
+}
+
+module.exports = {
+  init,
+  getRecipes,
+  getIngredients,
+  addRecipe,
+  editRecipe,
+  deleteRecipe
+}
